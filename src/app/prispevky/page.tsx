547ae58ef5cf2750dbdb20dfe27d423133f51fac
MarekMikudik <marekmikudik@s.zochova.sk>
2024-11-27 12:02:18 +0100

    prispevky

---
+++ src/app/prispevky/page.tsx
@@ -0,0 +1,16 @@
+// src/app/prispevky/page.tsx
+
+
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+export const metadata = { title: "Príspevky | ZoškaSnap" };
+
+export default function Add() {
+
+  return (
+    <Container>
+      <Typography> Príspevky</Typography>
+    </Container>
+  );
+}
